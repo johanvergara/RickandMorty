e2efcb2aef76c4732a4d15651ea409bbe0e60156
Johan Vergara <vjoha647@gmail.com>
2021-04-11 17:06:41 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
+import React, {Fragment} from 'react';
 import './App.css';
+import logo from  './img/Logo.png';
+import ImagenCentral from  './img/image-center.png';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <Fragment>
+
+      {/* Header */}
+      <header>
+        <div className="logo-container">
+          <img src={logo} alt="Logo Ricky and Morty" />
+        </div>
+        {/* Menu */}
+        <nav>
+          <ul className="nav-links">
+            <li className="nav-link">Inicio</li>
+            <li className="nav-link">Episodios</li>
+            <li className="nav-link">Doc</li>
+          </ul>
+        </nav>
       </header>
-    </div>
+
+      {/* Container */}
+      <main>
+        <section className="presentation">
+          <div className="introduction">
+            <div className="intro-text">
+              <h1>Rick & Morty Episodios</h1>
+              <p>Todos tus episodios favoritos de ricky and morty</p>
+            </div>
+            <div>
+              <div className="search">
+                <input type="text" placeholder=" Search..." className="buttom-search" />
+                {/* <button type="button" className="cta-select">Search</button> */}
+              </div>
+            </div>
+          </div>
+          <div className="cover">
+            <img src={ImagenCentral} alt="imagen principal" />
+          </div>
+        </section>
+      </main>
+
+    </Fragment>
   );
 }
 
